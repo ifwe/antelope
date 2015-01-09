47ab352f387b40adadc22ab8f7c9d41ef135704a
Johann Schleier-Smith <johann@ifwe.co>
2015-01-09 10:00:01 -0800

    working on spelling

--- demo-best-buy/src/main/scala/co/ifwe/antelope/bestbuy/exec/LearnedRankerScoring.scala
+++ demo-best-buy/src/main/scala/co/ifwe/antelope/bestbuy/exec/LearnedRankerScoring.scala
@@ -1,8 +1,8 @@
 package co.ifwe.antelope.bestbuy.exec
 
-import co.ifwe.antelope.bestbuy.{EventProcessing, RecommendationStats, ModelEventProcessor}
+import co.ifwe.antelope.bestbuy.{MissAnalysis, EventProcessing, RecommendationStats, ModelEventProcessor}
 import co.ifwe.antelope.Event
-import co.ifwe.antelope.bestbuy.event.ProductView
+import co.ifwe.antelope.bestbuy.event.{ProductUpdate, ProductView}
 
 /**
  * Score a some of the documents based on training data - run this
@@ -12,6 +12,7 @@ object LearnedRankerScoring extends App with EventProcessing {
   override def productViewLimit(): Int = 40000
   override protected def getEventProcessor() = {
     val rs = new RecommendationStats()
+    val ma = new MissAnalysis()
 
     new ModelEventProcessor(
         weights = Array(88.77053,2437.086,0.1170896,7670.614,-0.02810121),
@@ -24,14 +25,20 @@ object LearnedRankerScoring extends App with EventProcessing {
             viewCt += 1
             if (viewCt > 30000) {
               val td = topDocs(pv.query, 5)
-              rs.record(pv.skuSelected, td)
+              val hit = rs.record(pv.skuSelected, td)
+              if (!hit) {
+                ma.miss(pv, td)
+              }
             }
+          case pu: ProductUpdate =>
+            ma.register(pu)
           case _ =>
         }
         super.consume(e)
       }
       override protected def postProcess() {
         println("%s finishing with stats: %s".format(m, rs))
+        println(ma.summarize())
         super.postProcess()
       }
     }
