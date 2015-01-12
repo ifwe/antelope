a6b27a606ae8a5b317011b0e3035b62d4e084501
Johann Schleier-Smith <johann@ifwe.co>
2015-01-12 09:14:33 -0800

    some improvements to text normalization

--- antelope/src/main/scala/co/ifwe/antelope/Text.scala
+++ antelope/src/main/scala/co/ifwe/antelope/Text.scala
@@ -4,12 +4,21 @@ package co.ifwe.antelope
  * Normalize input text, extract terms and bigrams
  */
 object Text {
-  private val cleanRegex = "[\"\\-\\./:,]".r
-  private val extraSpacesRegex = "\\s+".r
+  private val normalizers: List[String=>String] = List(
+    _.toLowerCase(),                            // lower case conversion
+    "(?:(?<=\\.|\\s|^)[a-z]\\.)+".r.              // convert acronyms to plain words
+      replaceAllIn(_,
+        x => x.group(0).replaceAll("\\.","")),
+    "[']".r.replaceAllIn(_, ""),                // drop characters and contract words
+    "[\"\\-\\./:,]".r.replaceAllIn(_, " "),     // drop characters to whitespace
+    (x: String) => x.trim,                      // remove leading or trailing whitespace
+    "\\s+".r.replaceAllIn(_," ")                // remove duplicate spaces
+  )
 
   type StringToTerms = String => Iterable[String]
 
-  def normalize(s: String): String = extraSpacesRegex.replaceAllIn(cleanRegex.replaceAllIn(s.toLowerCase," ").trim," ")
+  def normalize(s: String): String = normalizers.foldLeft(s)((x, n) => n(x))
+
   def termsExtract(s: String): Iterable[String] = normalize(s).split(" ")
   def bigramsExtract(s: String): Iterable[String] = termsExtract(s).sliding(2).map(_.mkString(" ")).toIterable
   def joinedBigramsExtract(s: String): Iterable[String] = termsExtract(s).sliding(2).map(_.mkString("")).toIterable
