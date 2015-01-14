bc4f56d56dc9d933e87bd22e16e0945269971cba
Johann Schleier-Smith <johann@ifwe.co>
2015-01-13 18:39:23 -0800

    fix input event ordering

--- antelope/src/main/scala/co/ifwe/antelope/EventProcessor.scala
+++ antelope/src/main/scala/co/ifwe/antelope/EventProcessor.scala
@@ -22,9 +22,28 @@ abstract class EventProcessor {
     onShutdown()
   }
 
+  private def confirmOrder(t: Traversable[Event]): Traversable[Event] = {
+    new Traversable[Event] {
+      override def foreach[U](f: (Event) => U): Unit = {
+        var lastTs: Long = 0
+        t.foreach {
+          e: Event =>
+            val ts = e.ts
+            // Give 1s allowance for out-of-order data
+            if (ts < lastTs - 1000) {
+              throw new RuntimeException(s"out of sequence data: $lastTs " +
+                s"comes before ${ts}, delta ${lastTs - ts} ms")
+            }
+            lastTs = e.ts
+            f(e)
+        }
+      }
+    }
+  }
+
   def process(es: Traversable[Event], limit: Int = 0) {
     if (limit > 0) {
-      es.take(limit).foreach(consume)
+      confirmOrder(es.take(limit)).foreach(consume)
     } else {
       es.foreach(consume)
     }
