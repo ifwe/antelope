8de1299dd86c8161c031368ae4760096297e715a
Johann Schleier-Smith <johann@ifwe.co>
2015-01-21 16:28:44 -0800

    inroduce date formatting utilities

--- demo-best-buy/src/main/scala/co/ifwe/antelope/bestbuy/IOUtil.scala
+++ demo-best-buy/src/main/scala/co/ifwe/antelope/bestbuy/IOUtil.scala
@@ -1,8 +1,23 @@
 package co.ifwe.antelope.bestbuy
 
 import java.io.{PrintWriter, BufferedWriter, FileWriter}
+import java.text.SimpleDateFormat
 
 object IOUtil {
+
+  val df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+  val backupDf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+  def getTime(timeStr: String): Long = {
+    try {
+      df.parse(timeStr).getTime
+    } catch {
+      case e: java.text.ParseException => backupDf.parse(timeStr).getTime
+    }
+  }
+
+  def getUser(userStr: String): Long = {
+    java.lang.Long.parseUnsignedLong(userStr.substring(0, 16), 16) & 0x7fffffffffffffffL
+  }
 
   trait Writer {
     def write(x: Iterable[String])
