f1d76d1d8317c2d967b43ea3acc3dde886c8390b
Johann Schleier-Smith <johann@ifwe.co>
2015-01-21 16:45:57 -0800

    add data preparation class

---
+++ demo-best-buy/src/main/scala/co/ifwe/antelope/bestbuy/exec/PrepareData.scala
@@ -0,0 +1,17 @@
+package co.ifwe.antelope.bestbuy.exec
+
+import co.ifwe.antelope.EventSource
+import co.ifwe.antelope.bestbuy.event.{Storage, ProductView}
+import co.ifwe.antelope.util.ProgressMeter
+
+object PrepareData extends App {
+  import co.ifwe.antelope.bestbuy.IOUtil._
+  val viewsFn = "/Users/johann/dev/aml/bestbuy/data_large/train_sorted.csv"
+  val viewsFnBin = viewsFn + ".bin"
+
+  val pm = new ProgressMeter()
+  val xx = EventSource.fromFile(viewsFn).view.map(e =>
+    new ProductView(getTime(e("click_time")), getTime(e("query_time")), getUser(e("user")), e("query"), e("sku").toLong))
+  Storage.writeEvents(viewsFnBin, xx)
+  pm.finished()
+}
