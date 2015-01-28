b9769fff5d8d2cff56b9fbc86ee2be8779158309
Johann Schleier-Smith <johann@ifwe.co>
2015-01-28 15:48:24 -0800

    remove old merge tests

--- demo-best-buy/src/main/scala/co/ifwe/antelope/bestbuy/exec/explore/MergeEventsWithUpdates.scala
+++
@@ -1,7 +0,0 @@
-package co.ifwe.antelope.bestbuy.exec.explore
-
-import FileLocations._
-
-object MergeEventsWithUpdates extends App {
-  Merge.merge(Array(mergedProducts, viewsFnBin), allEvents)
-}
